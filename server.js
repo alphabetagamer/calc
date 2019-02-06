cb19aa931949fa5fe11a3929e41c90e8f6179da4
alphabeta_g <34796341+alphabetagamer@users.noreply.github.com>
2019-02-06 11:07:19 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/calculator.html'));
+    
+    console.log();
+});
+app.post('/', (req, res) => {
+    const expr = req.body.expr;
+    
+    if(expr)
+    {
+        var c=0;
+        var i=expr.length;
+        for(j=0;j<i;j++)
+        {
+            if(isNaN(expr.charAt(j)))
+            {
+                c++;
+            }
+            else
+            {
+                c=0;
+            }
+            if(c>=2)
+            {
+                console.log("Wrong input");
+                break;
+            }
+            else if(j==i-1)
+            {
+                
+                var g=eval(expr);
+                var myCallback = function(data) {
+                    console.log(data);
+                  };
+                  
+                  var usingItNow = function(callback) {
+                    callback(g);
+                  };
+                  usingItNow(myCallback);
+                console.log("Accepted");
+                
+                
+                
+                
+            }
+
+        }
+    }
+    else
+    {
+        console.log("empty");
+    }
+    
+    
+  });
+app.listen(8080);
